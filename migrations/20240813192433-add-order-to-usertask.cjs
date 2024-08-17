eab87d97997a3f574562aed61ac623e216f13c8d
Dylan Erdini <erdini.dylan@gmail.com>
2024-08-17 17:10:33 -0300

    Migrando nuevos datos a la DB

---
+++ migrations/20240813192433-add-order-to-usertask.cjs
@@ -0,0 +1,16 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('UserTasks', 'order', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('UserTasks', 'order');
+  }
+};
